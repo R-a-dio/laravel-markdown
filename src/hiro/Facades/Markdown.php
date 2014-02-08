66c21365c8aa06ce8166eee1ccba2970b9d89272
David Collinson <hiroto@hiroto.eu>
2014-02-08 20:56:35 +0000

    Add laravel facade

---
+++ src/hiro/Facades/Markdown.php
@@ -0,0 +1,10 @@
+<?php namespace hiro\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Markdown extends Facade {
+
+	protected static function getFacadeAccessor() {
+		return "markdown";
+	}
+}
